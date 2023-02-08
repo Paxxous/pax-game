0cf96576e21e5007da006d3053f3cba7a24d57fa
Benjamin <itsenveehio@gmail.com>
2023-02-07 21:11:53 -0500

    version define is originally ASF, so I changed it to CURRENT_VERSION to improve readability. Then I explain why there's a colon in the gitignore.

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char* argv[/* its' cool in here */]) {
   engine::entity e(0.023, 213); // Yahoo! we got a pos system going :)
   std::cout << e.get_pos().x << " | " << e.get_pos().y << "\n";
 
-  std::cout << ASF << "\n";
+  std::cout << MAIN_VERSION << "\n";
 
   engine::end();
   return 0;
